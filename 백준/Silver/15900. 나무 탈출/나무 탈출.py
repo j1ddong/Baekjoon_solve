8348bdc96e02fc7be146cb921a502a020e67c61f
j1ddong <97656259+j1ddong@users.noreply.github.com>
2022-07-19 21:56:59 +0900

    [Silver I] Title: 나무 탈출, Time: 1808 ms, Memory: 844904 KB -BaekjoonHub

---
+++ 백준/Silver/15900. 나무 탈출/나무 탈출.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(500000)
+
+
+def dfs(node, cnt):
+    global ans
+    if not visited[node]:
+        visited[node] = 1
+        if node != 1 and len(arr[node]) == 1:
+            ans += cnt
+        for elem in arr[node]:
+            dfs(elem, cnt + 1)
+
+
+N = int(input())
+arr = [[] for _ in range(N + 1)]
+ans = 0
+for _ in range(N - 1):
+    a, b = map(int, input().split())
+    arr[a].append(b)
+    arr[b].append(a)
+
+visited = [0] * (N + 1)
+dfs(1, 0)
+if ans % 2:
+    print('Yes')
+else:
+    print('No')
