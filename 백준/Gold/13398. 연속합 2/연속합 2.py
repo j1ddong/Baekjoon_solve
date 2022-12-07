20340816360de7827704e16081ab42b6544fbc20
j1ddong <97656259+j1ddong@users.noreply.github.com>
2022-12-07 23:39:16 +0900

    [Gold V] Title: 연속합 2, Time: 156 ms, Memory: 38440 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.py
+++ 백준/Gold/13398. 연속합 2/연속합 2.py
@@ -1,18 +1,18 @@
 import sys
 
-if __name__ == '__main__':
-    N = int(input())
-    arr = list(map(int, sys.stdin.readline().split()))
-    dp = [[0] * N for _ in range(2)]
+N = int(input())
+arr = list(map(int, input().split()))
+# 제거 무 최대합, 제거 유 최대합
+dp = [[0] * N for _ in range(2)]
+dp[0][0] = arr[0]
 
-    dp[0][0] = arr[0] # 1개는 반드시 선택해야 한다.
+if N > 1:
+    max_number = -1e9
+    for i in range(1, N):
+        dp[0][i] = max(arr[i], dp[0][i - 1] + arr[i])
+        dp[1][i] = max(dp[0][i - 1], dp[1][i - 1] + arr[i])
 
-    if N > 1:
-        ans = -1e9
-        for i in range(1, N):
-            dp[0][i] = max(dp[0][i - 1] + arr[i], arr[i])
-            dp[1][i] = max(dp[0][i - 1], dp[1][i-1] + arr[i])
-            ans = max(ans, dp[0][i], dp[1][i])
-        print(ans)
-    else: # N이 1인 경우는 반드시 선택을 해야하므로 dp[0][0] 출력
-        print(dp[0][0])
+        max_number = max(max_number, dp[0][i], dp[1][i])
+    print(max_number)
+else:
+    print(dp[0][0])
