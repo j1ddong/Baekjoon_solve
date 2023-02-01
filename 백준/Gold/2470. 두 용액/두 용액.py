e62d9388b141263b5e46d965d18225d79b4a9afc
j1ddong <97656259+j1ddong@users.noreply.github.com>
2023-02-01 12:36:37 +0900

    [Gold V] Title: 두 용액, Time: 136 ms, Memory: 42172 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -1,22 +1,21 @@
-import sys
-input = sys.stdin.readline
 N = int(input())
 arr = list(map(int, input().split()))
 arr.sort()
-ans = 3000000000
-left, right = 0, len(arr) - 1
-al = ar = 0
+start, end = 0, len(arr) - 1
+ans = 2_000_000_000
+left, rigth = 0, 0
 
+while start < end:
+    curr = arr[start] + arr[end]
+    if abs(curr) < ans:
+        ans = abs(curr)
+        left, right = arr[start], arr[end]
+    if curr == 0:
+        left, right = arr[start], arr[end]
+        break
+    elif curr > 0:
+        end -= 1
+    elif curr < 0:
+        start += 1
 
-while left < right:
-    temp = arr[right] + arr[left]
-    if abs(temp) < ans:
-        ans = abs(temp)
-        al, ar = arr[left], arr[right]
-    if temp > 0:
-        right -= 1
-    else:
-        left += 1
-
-
-print(al, ar)
+print(left, right)
